bdce9487d080f8e27dd452ea6ce7b749a43ad414
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2023-12-20 22:22:05 +0530

    add timestamp feature

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const notesContainer = document.getElementById("notesContainer");
 
 function createNote(newNote) {
     // If noteDatabase not found then create
@@ -46,9 +45,10 @@ function deleteAll()
 function createNoteBtn() {
     const noteName = document.getElementById("note_name");
     const noteContent = document.getElementById("note_content");
-    randomId = Math.floor(Math.random() * 9999) + 1;
+    let randomId = Math.floor(Math.random() * 9999) + 1;
+    let unixTime = Math.floor(Date.now() / 1000);
 
-    const newNote = { id: randomId, title: noteName.value, content: noteContent.value }
+    const newNote = { id: randomId, title: noteName.value, content: noteContent.value, timestamp: unixTime }
 
     createNote(newNote);
     
@@ -56,12 +56,17 @@ function createNoteBtn() {
 }
 
 function getAllNotes() {
+    const notesContainer = document.getElementById("notesContainer");
+
     notesContainer.innerHTML = "";
     const notesData = JSON.parse(localStorage.getItem('notesData'));
 
     notesData.forEach(note => {
 
         randomColor = Math.floor(Math.random() * 4) + 1;
+
+        timestamp = getFormattedDateTime(note.timestamp)
+
         const rawHTML = `<div class="col">
         <div class="card notes-card color-${randomColor}">
             <div class="card-body notes-body">
@@ -77,6 +82,12 @@ function getAllNotes() {
                 </div>
                  
                 <p class="notes-text">${note.content}</p>
+
+            </div>
+            <div class="date">
+            <span><i class="bi bi-calendar"></i> ${timestamp.date}</span>
+            <span><i class="bi bi-clock"></i> ${timestamp.time}</span>
+            <span onclick="copyTo()"><i class="bi bi-copy"></i></span>
             </div>
         </div>
         </div>`;
@@ -99,5 +110,31 @@ function deleteNote(noteId) {
 
 }
 
+function copyTo()
+{
+
+}
+
+function getFormattedDateTime(unixTimestamp) {
+    const date = new Date(unixTimestamp * 1000); // Convert Unix timestamp to milliseconds
+
+    const day = date.getDate(); // Get the day of the month (1-31)
+    const month = date.toLocaleString('default', { month: 'short' }); // Get the month abbreviation (e.g., Jan)
+    const year = date.getFullYear(); // Get the year (four digits)
+    let hours = date.getHours(); // Get the hours (0-23)
+    const minutes = ('0' + date.getMinutes()).slice(-2); // Get the minutes with leading zero if needed
+
+    // Convert hours to 12-hour format and determine AM/PM
+    const amPM = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12 || 12; // Convert hours from 24-hour to 12-hour format
+
+    const formattedDate = `${day} ${month} ${year}`;
+    const formattedTime = `${hours}:${minutes} ${amPM}`;
+
+    return {
+        date: formattedDate,
+        time: formattedTime
+    };
+}
 
 getAllNotes();
